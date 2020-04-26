2ecd9d679c911b75a446a92f623a62725f43479a
Tobias Rosenberg <mail@trosenberg.com>
2020-04-26 16:44:39 +0200

    Initial commit from gatsby: (https://github.com/TobsDev/gatsby-boilerplate.git)

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../scss/main.scss'
+
+
+class IndexPage extends React.Component {
+  render () {
+    return (
+      <div className="indexPage">
+        Boilerplate, ready to go
+      </div>
+    );
+  }
+};
+
+export default IndexPage
